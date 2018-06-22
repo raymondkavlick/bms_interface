cd581d5a8171d3c0e8ee4411fabefa78c744f7b5
Rk <Rk>
2018-06-22 11:18:11 -0700

    Uploading to github for file transfer. WIP.

--- bms_interface.py
+++ bms_interface.py
@@ -197,8 +197,6 @@ class worker(QtCore.QObject):
                                 self.signalCatchAll.emit("regen")
                             elif PackCurrent.value < -10:
                                 self.signalCatchAll.emit("discharge")
-                                
-                            print PackCurrent.value
                         elif msg.ID == 0x2AD:
                             MaxCellVolts = msg.DATA[0] + (msg.DATA[1] * 256)#endian
                             MinCellVolts = msg.DATA[2] + (msg.DATA[3] * 256)#endian
