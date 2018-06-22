829f92207a3b2d008a3fa4199e26e4b8a6baae5e
Rk <Rk>
2018-06-22 11:12:18 -0700

    Uploading to github for file transfer. WIP.

--- ui_helper.py
+++ ui_helper.py
@@ -64,6 +64,6 @@ class Ui_Dialog_Derived(bms_ui_form.Ui_Dialog):
             self.connectButton.setEnabled(False)
             self.powerDownButton.setEnabled(True)
         elif action == "regen":
-            self.textEdit_4.setTextBackgroundColor(0,255,0)
+            self.textEdit_4.setTextBackgroundColor(QtGui.QColor(0,255,0))
         elif action == "discharge":
-            self.textEdit_4.setTextBackgroundColor(255,255,0)
+            self.textEdit_4.setTextBackgroundColor(QtGui.QColor(255,0,0))
